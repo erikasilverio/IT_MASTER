50b3fc9484afc8b60b5b552869f24f9d9e947892
Erika Silverio <cms.silverio@gmail.com>
2023-06-11 18:44:23 -0300

    COMMIT

---
+++ clase.0102/segundo.py
@@ -0,0 +1,25 @@
+"""
+
+ LERR 2 NUMEROS Y MOSTRAR LA SUMA
+
+ Inicio del programa
+
+"""
+
+
+numero1 = input("Ingrese un numero: ")
+
+numero1 = float(numero1)
+
+
+numero2 = int(input("Ingrese un numeo: "))
+
+suma = numero1 + numero2
+
+print("suma: ", suma)
+print("suma: ", numero1 + numero2)
+
+
+x = suma/5
+
+print(x)
