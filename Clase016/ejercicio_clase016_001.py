f68a64aeda9eda66230c7a6b01bb6a559834cc54
Erika Silverio <cms.silverio@gmail.com>
2023-07-06 01:07:04 -0300

    COMMIT

--- Clase016/ejercicio_clase016_001.py
+++ Clase016/ejercicio_clase016_001.py
@@ -2,8 +2,33 @@
 
 
 
+def llenar_desde_archivo(nombre_archivo):
+    lista = []
+
+    archivo = open(file=nombre_archivo,mode='r',encoding='utf-8')
+
+    linea = archivo.readline()
+    while linea != '':
+        print(linea)
+
+        linea = archivo.readline()
+
+    archivo.close()
+
+
+
+    return lista
+
+
+
+CAMINO = "Clase016/"
+
 def main():
-    lista = llenar_desde_archivo("numeros.txt")
+    lista = llenar_desde_archivo(CAMINO + "numeros.txt")  
+    
+
+
+    # para encontrar archivo otra manera lista = llenar_desde_archivo("Clase016\\numeros.txt")
 
 
 
