c6d12afe3f71ef9a18dffd2064c87705d21e30e2
Erika Silverio <cms.silverio@gmail.com>
2023-07-18 19:08:50 -0300

    commit

--- Clase020/crear_persona.py
+++ Clase020/crear_persona.py
@@ -1,5 +1,5 @@
 
-
+import random
 
 # dni,apellido,nombre,sexo,edad,provincia,localidad,calle,altura,ubicacion,mail
 
@@ -14,26 +14,31 @@ ARCHIVO_APELLIDOS = "Apellidos.txt"
 
 
 
-def obter_lista(nombre_archivo:str)->list:
+def obtener_lista(nombre_archivo:str)->list:
     try:
 
-    except IOError:
+        with open(file=nombre_archivo,mode='r',encoding='utf-8') as archivo:
+            return [ linea.rstrip() for linea in archivo]
         
 
+    except IOError as e:  
+        raise RuntimeError(f"Error: {nombre_archivo}\n Pyton == > {e}")
 
 
 
 
 
-def crear_lista_persona(cantidad_persona:int)->list:
+
+def crear_lista_personas(cantidad_persona:int)->list:
     femininos = obtener_lista(ARCHIVO_NOMBRE_FEMININOS)
     masculinos = obtener_lista(ARCHIVO_NOMBRE_MASCULINOS)
-    apellidos = obter_lista(ARCHIVO_APELLIDOS)
+    apellidos = obtener_lista(ARCHIVO_APELLIDOS)
 
 
 
 
 def main():
+
     lista_personas = crear_lista_personas(10)
     
 
